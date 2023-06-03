ab1b47c37c6b5ceea5b5f6ad98e29d8a5e1d9715
kirankumarsanthipuram@gmail.com <kirankumarsanthipuram@gmail.com>
2023-06-03 10:16:19 +0530

    first commit intro

---
+++ lib/screens/intro.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyIntro extends StatelessWidget {
+  const MyIntro({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
